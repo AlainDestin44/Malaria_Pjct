d0c6aede6e9244b604a578e5901203988ba58e5c
Alain Destin Karasira <nishkaralain@gmail.com>
2024-08-18 23:14:22 +0200

    version 2

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from src.webapp.app import create_app
-from src.webapp.routes import main
-from src.config import MODEL_PATH,OUTPUT_NAME,CLASS_NAMES,session,SEVERITY_THRESHOLD,INPUT_NAME
-
-# print(CLASS_NAMES)
-if __name__ == "__main__":
-    # Create an instance of the Flask app
-    app = create_app()
-    app.run(debug=True)
--- src/webapp/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from src.webapp.routes import init_routes
-# from src import init_routes
-
-def create_app():
-    app = Flask(__name__)
-    init_routes(app)
-    return app
-
